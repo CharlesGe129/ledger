0589abf8794ff937965b0f6a432a0b490ffa840b
CharlesGe129 <char1209ge@gmail.com>
2019-07-14 19:53:19 +0800

    Fix ForeignKey select

--- pkg/server/item.go
+++ pkg/server/item.go
@@ -10,7 +10,7 @@ import (
 
 func GetItems(c *gin.Context) (items []models.Item) {
 	db := database.GetDB()
-	if err := db.Table("items").Joins("JOIN categories ON items.category_id=categories.id").Find(&items).Error; err != nil {
+	if err := db.Find(&items).Error; err != nil {
 		fmt.Printf("err=%s", err)
 	}
 	return
@@ -19,7 +19,7 @@ func GetItems(c *gin.Context) (items []models.Item) {
 func GetApiItems(c *gin.Context) {
 	var items []models.Item
 	db := database.GetDB()
-	if err := db.Table("items").Joins("JOIN categories ON items.category_id=categories.id").Find(&items).Error; err != nil {
+	if err := db.Preload("Category").Find(&items).Error; err != nil {
 		fmt.Printf("err=%s", err)
 	} else {
 		c.JSON(http.StatusOK, items)
